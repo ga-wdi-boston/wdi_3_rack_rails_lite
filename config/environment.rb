77e929cc5830d64433e327b760cf462286eb737f
Tom Dyer <tdyer@ourstage.com>
2014-06-24 14:24:00 -0400

    require erubis in the environment.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,10 @@ require 'pry'
 $RAILS_ROOT = "#{__FILE__.split('/')[0..-3].join('/')}"
 
 # Get active support help
-#require 'active_support/all'
+require 'active_support/all'
+
+# For view templates.
+require 'erubis'
 
 # Require all of the rails files in these directories                          
 Dir["#{$RAILS_ROOT}/app/controllers/**/*.rb"].each { |f| require(f) }
@@ -13,6 +16,7 @@ Dir["#{$RAILS_ROOT}/app/models/**/*.rb"].each { |f| require(f) }
 Dir["#{$RAILS_ROOT}/lib/**/*.rb"].each { |f| require(f) }
 
 require_relative "./application.rb"
+
 
 
 
