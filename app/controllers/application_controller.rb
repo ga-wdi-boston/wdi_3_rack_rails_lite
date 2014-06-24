bbe067e9d101b3aa37236fa2a8460fd79e6b8668
Tom Dyer <tdyer@ourstage.com>
2014-06-24 08:19:44 -0400

    Implement the render method in the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ module PersonApp
     LAYOUT_HTML_POST = '</body></html>'
 
     attr_accessor :params
-    
+
     def initialize()
       @params = {}
     end
@@ -13,6 +13,18 @@ module PersonApp
     def path_to_params(path)
       id = path.split("/").last.to_i
       @params.merge!({id: id})
+    end
+
+    def render(object)
+      content = LAYOUT_HTML_PRE
+      if object.respond_to?(:each)
+        object.each do |model|
+          content += model.to_html          
+        end
+      else
+        content += object.to_html
+      end
+      content += LAYOUT_HTML_POST
     end
   end
 end
